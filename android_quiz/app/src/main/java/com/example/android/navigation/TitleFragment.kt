482995094abecd03ab6c6dd79dde6767ebf9c0fb
Flavio Santana <fvvsantana@gmail.com>
2021-05-13 11:29:36 -0300

    [L3 9] Change navigation onClickListener

--- android_quiz/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android_quiz/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -23,9 +23,12 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
         // return inflater.inflate(R.layout.fragment_title, container, false)
+        /*
         binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+         */
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 
