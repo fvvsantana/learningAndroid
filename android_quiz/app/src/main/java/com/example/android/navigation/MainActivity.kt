4b4f99b7ff30b7314c7b03c64346ec8318e0ce1d
Flavio <fvvsantana@gmail.com>
2021-07-08 21:48:54 -0300

    [L3 27] Change drawer behavior using navigation listener

--- android_quiz/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ android_quiz/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -35,6 +37,15 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
+
+        navController.addOnDestinationChangedListener{ controller: NavController, destination: NavDestination, arguments: Bundle? ->
+            if(destination.id == controller.graph.startDestination){
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            }else{
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+            
+        }
 
     }
 
